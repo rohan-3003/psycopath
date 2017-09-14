c258ba246932e0d612a31d5a3caf11080be1aee4
rohan-3003 <rohansingh3003@gmail.com>
2017-09-15 01:30:01 +0530

    find it

--- app/src/main/java/com/example/dell/judge/attendance/AttendanceManagerAdapter.java
+++ app/src/main/java/com/example/dell/judge/attendance/AttendanceManagerAdapter.java
@@ -1,9 +1,11 @@
 package com.example.dell.judge.attendance;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import com.example.dell.judge.R;
 import com.example.dell.judge.schedule.ScheduleVariable;
@@ -13,11 +15,12 @@ import java.util.ArrayList;
  * Created by dell on 9/14/2017.
  */
 public class AttendanceManagerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-public ArrayList<AttendanceManagerModel> sm;
-        ScheduleVariable setV = new ScheduleVariable();
 
-public AttendanceManagerAdapter(ArrayList<AttendanceManagerModel> sm) {
-        this.sm = sm;
+    ArrayList<Character> attendanceArray = new ArrayList<>();
+public ArrayList<AttendanceManagerModel> attendAdapt;
+
+public AttendanceManagerAdapter(ArrayList<AttendanceManagerModel> attendAdapt) {
+        this.attendAdapt = attendAdapt;
         }
 
 @Override
@@ -30,30 +33,39 @@ public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType
 
 @Override
 public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-final ScheduleHolder sh = (ScheduleHolder) holder;
-        sh.studentView.setText(sm.get(position).getstudentName());
-//        sh.cd.setOnClickListener(new View.OnClickListener() {
-//@Override
-//public void onClick(View view) {
-//        setV.setVar(sh.dayy.getText().toString());
-//        ChooseFragment.setF("DaySchedule");
-//        Intent i=new Intent(MyApplication.getContext(), MainActivity.class);
-//        MyApplication.getContext().startActivity(i);
-//        }
-//        });
+    final int pos=position;
+    final ScheduleHolder sh = (ScheduleHolder) holder;
+        sh.studentView.setText(attendAdapt.get(position).getstudentName());
+        sh.presentButton.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
+            attendanceArray.add(pos,'p');
+        }
+        });
+    sh.absentButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            attendanceArray.add(pos,'a');
+            Log.d("attend",attendanceArray.toString());
+        }
+    });
         }
 
 @Override
 public int getItemCount() {
-        return sm.size();
+        return attendAdapt.size();
         }
+
 public static class ScheduleHolder extends RecyclerView.ViewHolder{
     private TextView studentView;
     private CardView studentCd;
+    private Button presentButton,absentButton;
     public ScheduleHolder(View itemView) {
         super(itemView);
         studentView=(TextView) itemView.findViewById(R.id.student);
         studentCd=(CardView) itemView.findViewById(R.id.studentCard);
+        presentButton=(Button) itemView.findViewById(R.id.present);
+        absentButton=(Button) itemView.findViewById(R.id.absent);
     }
 }
 }
